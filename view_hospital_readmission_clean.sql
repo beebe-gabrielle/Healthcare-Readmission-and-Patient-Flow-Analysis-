f7f79c3950a5a0d026966b6c263895b109078ebf
Gabrielle Beebe <beebegabrielle@gmail.com>
2026-05-07 11:54:04 -0400

    Update view_hospital_readmission_clean.sql

--- view_hospital_readmission_clean.sql
+++ view_hospital_readmission_clean.sql
@@ -1,25 +1,20 @@
+-- creating view of updated, clean data 
 CREATE VIEW hospital_readmission_clean AS
 SELECT patient_id,
-	   admission_date_clean,
-       season_clean,
-       age_clean,
-       gender_clean,
-       region_clean,
-       primary_diagnosis_clean,
-       comorbidities_count_clean,
-       length_of_stay_clean,
-       treatment_type_clean,
-       medications_count_clean,
-       followup_visits_clean,
-       prev_readmission_clean,
-       insurance_type_clean,
-       label_clean
+	   admission_date_clean AS admission_date,
+       season_clean AS season,
+       age_clean AS age,
+       gender_clean AS gender,
+       region_clean AS region,
+       primary_diagnosis_clean AS primary_diagnosis,
+       comorbidities_count_clean AS comorbidities_count,
+       length_of_stay_clean AS length_of_stay,
+       treatment_type_clean AS treatment_type,
+       medications_count_clean AS medications_count,
+       followup_visits_clean AS followup_visits,
+       prev_readmission_clean AS prev_readmissions,
+       insurance_type_clean AS  insurance_type,
+       label_clean AS label
 FROM hospital_readmission;
 
-SELECT COUNT(*) 
-FROM hospital_readmission_clean ;
-
-SELECT *
-FROM hospital_readmission_clean
-WHERE patient_id = 'P03097';
 
